df8eb544ff313393ae9cb44de072857df865a8f8
Andrus Adamchik <andrus@objectstyle.com>
2016-08-30 18:10:30 +0300

    App-specific configuration of connector factories #13
    * must keep default factories if no config is explicitly present

--- src/main/java/io/bootique/linkmove/LinkMoveFactory.java
+++ src/main/java/io/bootique/linkmove/LinkMoveFactory.java
@@ -5,49 +5,61 @@ import com.nhl.link.move.connect.Connector;
 import com.nhl.link.move.runtime.LmRuntime;
 import com.nhl.link.move.runtime.LmRuntimeBuilder;
 import io.bootique.linkmove.connector.IConnectorFactoryFactory;
+import io.bootique.linkmove.connector.JdbcConnectorFactoryFactory;
+import io.bootique.linkmove.connector.URIConnectorFactoryFactory;
 import org.apache.cayenne.configuration.server.ServerRuntime;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 public class LinkMoveFactory {
 
-	private String extractorsDir;
+    private String extractorsDir;
 
-	private List<IConnectorFactoryFactory<Connector>> connectorFactories;
+    private List<IConnectorFactoryFactory<? extends Connector>> connectorFactories;
 
-	public void setExtractorsDir(String extractorsDir) {
-		this.extractorsDir = extractorsDir;
-	}
 
-	public void setConnectorFactories(List<IConnectorFactoryFactory<Connector>> connectorFactories) {
-		this.connectorFactories = connectorFactories;
-	}
+    public LinkMoveFactory() {
+        this.extractorsDir = ".";
+        this.connectorFactories = new ArrayList<>();
 
-	public LinkMoveFactory() {
-		this.extractorsDir = ".";
-	}
+        // add default factories
+        connectorFactories.add(new JdbcConnectorFactoryFactory());
+        connectorFactories.add(new URIConnectorFactoryFactory());
+    }
 
-	public LmRuntime createLinkMove(Injector injector, ServerRuntime targetRuntime,
-									Set<LinkMoveBuilderCallback> builderCallbacks) {
+    public LmRuntime createLinkMove(Injector injector, ServerRuntime targetRuntime,
+                                    Set<LinkMoveBuilderCallback> builderCallbacks) {
 
-		Objects.requireNonNull(extractorsDir);
+        Objects.requireNonNull(extractorsDir);
 
-		LmRuntimeBuilder builder = new LmRuntimeBuilder().withTargetRuntime(targetRuntime).extractorModelsRoot(extractorsDir);
+        LmRuntimeBuilder builder = new LmRuntimeBuilder().withTargetRuntime(targetRuntime).extractorModelsRoot(extractorsDir);
 
-		connectorFactories.forEach(provider ->
-				builder.withConnectorFactory(provider.getConnectorType(), provider.getConnectorFactory(injector)));
+        connectorFactories.forEach(factory -> addToBuilder(builder, factory, injector));
 
-		builderCallbacks.forEach(c -> c.build(builder));
-		return builder.build();
-	}
+        builderCallbacks.forEach(c -> c.build(builder));
+        return builder.build();
+    }
 
-	public List<IConnectorFactoryFactory<Connector>> getConnectorFactories() {
-		return connectorFactories;
-	}
+    protected <C extends Connector> void addToBuilder(LmRuntimeBuilder builder, IConnectorFactoryFactory<C> factoryFactory, Injector injector) {
+        builder.withConnectorFactory(factoryFactory.getConnectorType(), factoryFactory.getConnectorFactory(injector));
+    }
 
-	public String getExtractorsDir() {
-		return extractorsDir;
-	}
+    public List<IConnectorFactoryFactory<? extends Connector>> getConnectorFactories() {
+        return connectorFactories;
+    }
+
+    public void setConnectorFactories(List<IConnectorFactoryFactory<? extends Connector>> connectorFactories) {
+        this.connectorFactories = connectorFactories;
+    }
+
+    public String getExtractorsDir() {
+        return extractorsDir;
+    }
+
+    public void setExtractorsDir(String extractorsDir) {
+        this.extractorsDir = extractorsDir;
+    }
 }
