64c71ac3184b9c2b96f491e515384c478514d987
Andrus Adamchik <andrus@objectstyle.com>
2017-10-05 19:03:26 +0300

    Extractor XML descriptors can't be loaded from .jar files #25
    * ignored test demonstrating the problem

--- src/test/java/io/bootique/linkmove/LinkMoveModule_ConfigurationIT.java
+++ src/test/java/io/bootique/linkmove/LinkMoveModule_ConfigurationIT.java
@@ -1,12 +1,16 @@
 package io.bootique.linkmove;
 
+import com.nhl.link.move.extractor.model.ExtractorModel;
+import com.nhl.link.move.extractor.model.ExtractorName;
+import com.nhl.link.move.runtime.LmRuntime;
+import com.nhl.link.move.runtime.extractor.model.IExtractorModelService;
 import io.bootique.config.ConfigurationFactory;
 import io.bootique.linkmove.connector.IConnectorFactoryFactory;
 import io.bootique.linkmove.connector.JdbcConnectorFactoryFactory;
 import io.bootique.linkmove.connector.URIConnectorFactoryFactory;
-import io.bootique.resource.FolderResourceFactory;
 import io.bootique.resource.ResourceFactory;
 import io.bootique.test.junit.BQTestFactory;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -14,6 +18,7 @@ import java.io.File;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class LinkMoveModule_ConfigurationIT {
@@ -24,6 +29,13 @@ public class LinkMoveModule_ConfigurationIT {
     private static void assertContainsEntry(Map<?, ResourceFactory> m, Object k, Object v) {
         assertTrue("Missing key: " + k, m.containsKey(k));
         assertEquals("Unexpected value for key: " + k, m.get(k).getResourceId(), v);
+    }
+
+    private LmRuntime runtime(String appConfig) {
+        return testFactory.app("-c", appConfig)
+                .autoLoadModules()
+                .createRuntime()
+                .getInstance(LmRuntime.class);
     }
 
     private LinkMoveFactory factory(String appConfig) {
@@ -56,13 +68,30 @@ public class LinkMoveModule_ConfigurationIT {
     @Test
     public void testConfiguration_ExtractorsDir_Classpath() {
 
-        LinkMoveFactory f = factory("classpath:io/bootique/linkmove/extractorsDirClasspath.yml");
+        LmRuntime runtime = runtime("classpath:io/bootique/linkmove/extractorsDirClasspath.yml");
 
+        ExtractorModel model = runtime
+                .service(IExtractorModelService.class)
+                .get(ExtractorName.create("extractor.xml", ExtractorModel.DEFAULT_NAME));
 
-        FolderResourceFactory folderResourceFactory = f.getExtractorsDir();
-        File file = new File(folderResourceFactory.getUrl().getFile(), "extractor.xml");
+        assertNotNull(model);
+        assertEquals(ExtractorModel.DEFAULT_NAME, model.getName());
+        assertEquals("jdbc", model.getType());
+    }
 
-        assertTrue(file.getAbsolutePath() + " is not a file", file.isFile());
+    @Test
+    @Ignore
+    public void testConfiguration_ExtractorsDir_Classpath_InJar() {
+
+        LmRuntime runtime = runtime("classpath:io/bootique/linkmove/extractorsInJar.yml");
+
+        ExtractorModel model = runtime
+                .service(IExtractorModelService.class)
+                .get(ExtractorName.create("extractor.xml", ExtractorModel.DEFAULT_NAME));
+
+        assertNotNull(model);
+        assertEquals(ExtractorModel.DEFAULT_NAME, model.getName());
+        assertEquals("jdbc", model.getType());
     }
 
     @Test
