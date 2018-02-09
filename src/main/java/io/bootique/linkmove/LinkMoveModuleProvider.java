62db666a537d780aefb22b73605f1db403b70f4d
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 15:47:35 +0300

    Declare module dependencies bootique/bootique#203

--- src/main/java/io/bootique/linkmove/LinkMoveModuleProvider.java
+++ src/main/java/io/bootique/linkmove/LinkMoveModuleProvider.java
@@ -3,30 +3,42 @@ package io.bootique.linkmove;
 import com.google.inject.Module;
 import io.bootique.BQModule;
 import io.bootique.BQModuleProvider;
-import io.bootique.cayenne.ServerRuntimeFactory;
+import io.bootique.cayenne.CayenneModuleProvider;
+import io.bootique.jdbc.JdbcModuleProvider;
 
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+
 public class LinkMoveModuleProvider implements BQModuleProvider {
 
-	@Override
-	public Module module() {
-		return new LinkMoveModule();
-	}
+    @Override
+    public Module module() {
+        return new LinkMoveModule();
+    }
 
-	@Override
-	public Map<String, Type> configs() {
-		// TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
-		// generate config prefix, reusing it in metadata...
-		return Collections.singletonMap("linkmove", LinkMoveFactory.class);
-	}
+    @Override
+    public Map<String, Type> configs() {
+        // TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
+        // generate config prefix, reusing it in metadata...
+        return Collections.singletonMap("linkmove", LinkMoveFactory.class);
+    }
 
-	@Override
-	public BQModule.Builder moduleBuilder() {
-		return BQModuleProvider.super
-				.moduleBuilder()
-				.description("Provides integration with LinkMove ETL framework.");
-	}
+    @Override
+    public BQModule.Builder moduleBuilder() {
+        return BQModuleProvider.super
+                .moduleBuilder()
+                .description("Provides integration with LinkMove ETL framework.");
+    }
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return asList(
+                new JdbcModuleProvider(),
+                new CayenneModuleProvider()
+        );
+    }
 }
--- src/test/java/io/bootique/linkmove/LinkMoveModuleProviderIT.java
+++ src/test/java/io/bootique/linkmove/LinkMoveModuleProviderIT.java
@@ -1,17 +1,37 @@
 package io.bootique.linkmove;
 
+import io.bootique.BQRuntime;
+import io.bootique.cayenne.CayenneModule;
+import io.bootique.jdbc.JdbcModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static com.google.common.collect.ImmutableList.of;
 
 public class LinkMoveModuleProviderIT {
 
-	@Test
-	public void testPresentInJar() {
-	    BQModuleProviderChecker.testPresentInJar(LinkMoveModuleProvider.class);
-	}
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
-	@Test
-	public void testMetadata() {
-		BQModuleProviderChecker.testMetadata(LinkMoveModuleProvider.class);
-	}
+    @Test
+    public void testPresentInJar() {
+        BQModuleProviderChecker.testPresentInJar(LinkMoveModuleProvider.class);
+    }
+
+    @Test
+    public void testMetadata() {
+        BQModuleProviderChecker.testMetadata(LinkMoveModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new LinkMoveModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+                JdbcModule.class,
+                LinkMoveModule.class,
+                CayenneModule.class
+        ));
+    }
 }
