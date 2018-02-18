feff3a402677a928742aac11cc0b6cca850e46a4
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:17:34 +0300

    Declare module dependencies bootique/bootique#203
    * updating test API

--- src/test/java/io/bootique/linkmove/LinkMoveModuleProviderIT.java
+++ src/test/java/io/bootique/linkmove/LinkMoveModuleProviderIT.java
@@ -4,11 +4,10 @@ import io.bootique.BQRuntime;
 import io.bootique.cayenne.CayenneModule;
 import io.bootique.jdbc.JdbcModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class LinkMoveModuleProviderIT {
 
@@ -28,10 +27,10 @@ public class LinkMoveModuleProviderIT {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new LinkMoveModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+        BQRuntimeChecker.testModulesLoaded(bqRuntime,
                 JdbcModule.class,
                 LinkMoveModule.class,
                 CayenneModule.class
-        ));
+        );
     }
 }
