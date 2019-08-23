2fe60c4489a10ecd7d3f9236492eda7c8fd3431a
Andrus Adamchik <andrus@objectstyle.com>
2019-08-23 09:18:03 +0300

    "bootique-linkmove-json" module #35
    * Cayenne 4.1 tests

--- bootique-linkmove-json-cayenne41/src/test/java/io/bootique/linkmove/json/LinkMoveJSONIT.java
+++ bootique-linkmove-json-cayenne41/src/test/java/io/bootique/linkmove/json/LinkMoveJSONIT.java
@@ -19,14 +19,55 @@
 
 package io.bootique.linkmove.json;
 
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.nhl.link.move.LmTask;
+import com.nhl.link.move.runtime.LmRuntime;
+import io.bootique.BQCoreModule;
+import io.bootique.cayenne.v41.CayenneModule;
+import io.bootique.cli.Cli;
+import io.bootique.command.Command;
+import io.bootique.command.CommandOutcome;
+import io.bootique.linkmove.json.cayenne.Table1;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 public class LinkMoveJSONIT {
 
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
+
     @Test
     public void testLinkMoveJSON() {
-        fail("TODO");
+        testFactory
+                .autoLoadModules()
+                .app("-c", "classpath:io/bootique/linkmove/json/test.yml")
+                .module(b -> BQCoreModule.extend(b).setDefaultCommand(LMWithJson.class))
+                .module(b -> CayenneModule.extend(b).addProject("io/bootique/linkmove/json/cayenne-project.xml"))
+                .run();
+    }
+
+    public static final class LMWithJson implements Command {
+
+        @Inject
+        private Provider<LmRuntime> runtime;
+
+        @Override
+        public CommandOutcome run(Cli cli) {
+
+            LmTask task = runtime.get()
+                    .getTaskService()
+                    .createOrUpdate(Table1.class)
+                    .sourceExtractor("extractor.xml")
+                    .task();
+
+            assertEquals(5, task.run().getStats().getCreated());
+            assertEquals(0, task.run().getStats().getCreated());
+
+            return CommandOutcome.succeeded();
+        }
     }
 }
