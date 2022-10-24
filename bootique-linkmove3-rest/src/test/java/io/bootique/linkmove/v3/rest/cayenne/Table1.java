2ed00882e2a9c9829587bcb0da3f394b085f4921
Andrus Adamchik <andrus@objectstyle.com>
2022-10-24 17:15:29 +0200

    New module structure and support for LM 3.x #50
    * link-move3-rest

---
+++ bootique-linkmove3-rest/src/test/java/io/bootique/linkmove/v3/rest/cayenne/Table1.java
@@ -0,0 +1,9 @@
+package io.bootique.linkmove.v3.rest.cayenne;
+
+import io.bootique.linkmove.v3.rest.cayenne.auto._Table1;
+
+public class Table1 extends _Table1 {
+
+    private static final long serialVersionUID = 1L;
+
+}
